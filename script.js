269fe230396da35978de4cfef4550e6cd047c8e1
jagannath p s <110271239+jagannath-p-s@users.noreply.github.com>
2023-07-15 18:10:46 +0530

    sharing the code for responsive navbar of my portfolio website
    feel free to use my responsive navbar in your projects , star the project if you like it , also reach out to me for proposing changes to the code or design

---
+++ script.js
@@ -0,0 +1,32 @@
+//  Header showing animation funtion starts here
+
+document.addEventListener("DOMContentLoaded", function () {
+    var header = document.getElementById("header");
+  
+    setTimeout(function () {
+      header.classList.add("header-show");
+    }, 0);
+  });
+  
+  //  Header showing animation funtion ends here
+  
+  //  menubar showing funtion starts here
+  
+  function toggleMenu() {
+    var header = document.getElementById("header");
+    header.classList.toggle("menu-open");
+
+    var menuIcon = document.querySelector(".menu-icon-image");
+
+  if (menuIcon.src === "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAv0lEQVR4nO2VXQrCMBAGxwtalZY86NkVLP6hN6hQCaQgJdXdsFHEDOQt+02TbBooFP6JBrgBB2ChqFsCJ+AK1FrpLBT2YXSAE9S5MHeo8xlqLk8BEvlY6sc5RbyKBE3JY9IuZCRRRwLvwGbUC7E561SpRJ5N+m4rpUdhvvJsK5XKs0qZONNYw31E2ueUu280V/Piykjuubl0wFxeGfwyfYaa1uCR2KeIjwbPos9QU4Uv3iq3zM/dhdp5irhQ+E0ekyummbane5EAAAAASUVORK5CYII=") {
+    menuIcon.src = "./assets/menuIcon.svg";
+  } else {
+    menuIcon.src = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAB4AAAAeCAYAAAA7MK6iAAAACXBIWXMAAAsTAAALEwEAmpwYAAAAv0lEQVR4nO2VXQrCMBAGxwtalZY86NkVLP6hN6hQCaQgJdXdsFHEDOQt+02TbBooFP6JBrgBB2ChqFsCJ+AK1FrpLBT2YXSAE9S5MHeo8xlqLk8BEvlY6sc5RbyKBE3JY9IuZCRRRwLvwGbUC7E561SpRJ5N+m4rpUdhvvJsK5XKs0qZONNYw31E2ueUu280V/Piykjuubl0wFxeGfwyfYaa1uCR2KeIjwbPos9QU4Uv3iq3zM/dhdp5irhQ+E0ekyummbane5EAAAAASUVORK5CYII=";
+  }
+  }
+
+
+  
+  //  menubar showing funtion ends here
+  
+        
